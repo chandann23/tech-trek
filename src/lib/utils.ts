38b688e03753fdc7d166d2496c7e3aa9b778069b
unknown <you@example.com>
2023-11-10 12:01:11 +0530

    fixed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path
   if (process.env.VERCEL_URL)
-    return `https://tech-trek-wheat.vercel.app/${path}`
+    return `https://pdfileai.vercel.app${path}`
   return `http://localhost:${
     process.env.PORT ?? 3000
   }${path}`
